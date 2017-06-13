5f61ad76bced079fa7133770d2d08a604adadd84
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-06-12 20:54:34 -0600

    fixed database

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'Simbit09&');
 
 /** MySQL hostname */
-define('DB_HOST', 'simbitgt.com');
+define('DB_HOST', '45.55.93.186');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
